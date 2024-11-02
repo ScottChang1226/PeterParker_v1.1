89d5572bd012a8abe1b1c0cfac3f461d3b248e95
peter5422000 <peter5422000@gmail.com>
2024-11-02 14:24:43 +0800

    Add files via upload

---
+++ backend/src/main/java/com/tibame/peterparker/dto/SpaceRequest.java
@@ -0,0 +1,14 @@
+package com.peterparker.dto;
+
+public class SpaceRequest {
+    private Integer parkingId;
+
+    // Getters and Setters
+    public Integer getParkingId() {
+        return parkingId;
+    }
+
+    public void setParkingId(Integer parkingId) {
+        this.parkingId = parkingId;
+    }
+}
