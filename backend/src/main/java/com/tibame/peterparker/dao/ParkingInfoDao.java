e510223ed5bdafaf383b5f662c0ba5f2ff29e4a0
peter5422000 <peter5422000@gmail.com>
2024-11-02 14:23:43 +0800

    Add files via upload

---
+++ backend/src/main/java/com/tibame/peterparker/dao/ParkingInfoDao.java
@@ -0,0 +1,19 @@
+package com.peterparker.dao;
+
+import com.peterparker.dto.ParkingInfoRequest;
+import com.peterparker.model.ParkingInfo;
+
+import java.util.List;
+
+public interface ParkingInfoDao {
+
+    ParkingInfo getParkingInfoByOwnerAndId(Integer ownerNo, Integer parkingId);
+    Integer createParkingInfo(ParkingInfoRequest parkingInfoRequest);
+    void updateParkingInfo(Integer ownerNo, Integer parkingId, ParkingInfoRequest parkingInfoRequest);
+    void deleteParkingInfoByOwnerAndId(Integer ownerNo, Integer parkingId);
+    List<ParkingInfo> getAllParkingInfoByOwnerNo(Integer ownerNo);  // 查询所有停车场信息
+    ParkingInfo getParkingInfoById(Integer parkingId);
+    void saveParkingImage(Integer ownerNo, Integer parkingId, byte[] imageData);
+    byte[] getParkingImage(Integer ownerNo, Integer parkingId); // 新增获取图片数据的方法
+
+}
