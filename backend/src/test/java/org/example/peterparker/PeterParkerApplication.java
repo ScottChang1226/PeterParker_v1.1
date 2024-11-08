63b6503e8fb8814e8c319faecb2652cf7ff65c3a
peter5422000 <peter5422000@gmail.com>
2024-11-08 21:58:36 +0800

    Add files via upload

---
+++ backend/src/test/java/org/example/peterparker/PeterParkerApplication.java
@@ -0,0 +1,11 @@
+package org.example.peterparker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterParkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PeterParkerApplication.class, args);
+    }
+}
